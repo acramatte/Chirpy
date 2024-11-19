669a971c7c6789baf21a5f9d53c9b57695f4fc85
Alexis Cramatte <alexis.cramatte@gmail.com>
2024-11-19 18:39:55 +0800

    initial commit with fileserver

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"net/http"
+)
+
+func main() {
+	serveMux := http.NewServeMux()
+	serveMux.Handle("/", http.FileServer(http.Dir(".")))
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: serveMux,
+	}
+	err := server.ListenAndServe()
+	if err != nil {
+		return
+	}
+}
