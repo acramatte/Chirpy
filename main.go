c6fe7ab3a73624885c592fe9933326e335b6ec53
Alexis Cramatte <alexis.cramatte@gmail.com>
2024-11-19 19:15:22 +0800

    add /healthz check endpoint
    - add endpoint to report that the server is ready to serve traffic
    - move fileserver under /app/ path instead of root / to avoid conflict

--- main.go
+++ main.go
@@ -1,18 +1,24 @@
 package main
 
 import (
+	"log"
 	"net/http"
 )
 
 func main() {
 	serveMux := http.NewServeMux()
-	serveMux.Handle("/", http.FileServer(http.Dir(".")))
+	serveMux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+	serveMux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		_, err := w.Write([]byte("OK"))
+		if err != nil {
+			log.Fatal("Fail to write")
+		}
+	})
 	server := &http.Server{
 		Addr:    ":8080",
 		Handler: serveMux,
 	}
-	err := server.ListenAndServe()
-	if err != nil {
-		return
-	}
+	log.Fatal(server.ListenAndServe())
 }
